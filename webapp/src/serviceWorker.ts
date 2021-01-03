543ff0e68fafbc4ede23753700c8013d5be7d900
Hyungsun Song <happynut.dev@gmail.com>
2021-01-03 17:35:24 +0900

    fixup eslint

--- webapp/src/serviceWorker.ts
+++ webapp/src/serviceWorker.ts
@@ -19,7 +19,7 @@ const isLocalhost = Boolean(
   )
 )
 
-export default function register(): void {
+export default function register (): void {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(
@@ -56,7 +56,7 @@ export default function register(): void {
   }
 }
 
-function registerValidSW(swUrl: string) {
+function registerValidSW (swUrl: string) {
   navigator.serviceWorker
     .register(swUrl)
     .then(registration => {
@@ -87,7 +87,7 @@ function registerValidSW(swUrl: string) {
     })
 }
 
-function checkValidServiceWorker(swUrl: string) {
+function checkValidServiceWorker (swUrl: string) {
   // Check if the service worker can be found. If it can't reload the page.
   fetch(swUrl)
     .then(response => {
@@ -114,7 +114,7 @@ function checkValidServiceWorker(swUrl: string) {
     })
 }
 
-export function unregister(): void {
+export function unregister (): void {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.ready.then(registration => {
       registration.unregister()
